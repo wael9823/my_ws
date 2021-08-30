5cdcbd9a78bdb12549d837083799d927b7dd4b4f
Wael <mohd.wael9823@gmail.com>
2021-08-31 01:11:28 +0530

    Delete build directory

--- build/hello/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/wael/my_ws/src/hello
-# Build directory: /home/wael/my_ws/build/hello
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
